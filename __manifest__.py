77b789e39c00dfec21553208b86363a51f38721a
hhvida-ai <hhvida@gmail.com>
2025-09-23 15:01:48 -0600

    Add manifest for Óptica lens quoting module

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Óptica - Cotizador de Lentes",
+    "version": "17.0.1.0.0",
+    "summary": "Series de micas, precios por serie y wizard de cotización basado en graduación",
+    "category": "Custom/Optica",
+    "author": "Asistente - Óptica Zamora",
+    "website": "",
+    "license": "LGPL-3",
+    "depends": ["base", "product", "sale_management", "contacts"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/optica_series_views.xml",
+        "views/product_template_inherit_views.xml",
+        "views/graduacion_inherit_views.xml",
+        "views/cotizador_wizard_views.xml",
+        "views/menus.xml",
+],
+    "installable": True,    
+    "application": False,
+}
